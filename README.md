1264bfe2682569ab34a9db1ad477fca00c6f0073
acl13 <128934431+acl13@users.noreply.github.com>
2023-10-29 14:36:45 -0400

    complete exercise

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # magic-8-ball
 Random fortune generator created using JavaScript
+
+Completed as part of the dev30 curriculum from Parsity
---
+++ main.js
@@ -0,0 +1,36 @@
+var userName = prompt("What is your name?");
+
+if (userName == null || userName == '') {
+    console.log("Hello!");
+} else {
+    console.log(`Hello, ${userName}!`)
+}
+
+var userQuestion = prompt("What would you like to ask the Magic 8 Ball?");
+
+console.log(`${userName} has asked the Magic 8 Ball '${userQuestion}'`);
+
+
+var randomNumber = Math.floor(Math.random() * 8);
+
+var eightBall = "";
+
+if (randomNumber === 0) {
+    eightBall = "It is certain";
+} else if ( randomNumber === 1) {
+    eightBall = "It is decidedly so";
+} else if ( randomNumber === 2) {
+    eightBall = "Reply hazy, try again";
+} else if ( randomNumber === 3) {
+    eightBall = "Cannot predict now";
+} else if ( randomNumber === 4) {
+    eightBall = "Do not count on it";
+} else if ( randomNumber === 5) {
+    eightBall = "My sources say no";
+} else if ( randomNumber === 6) {
+    eightBall = "Outlook not so good";
+} else if ( randomNumber === 7) {
+    eightBall = "Signs point to yes";
+}
+
+console.log(`The Magic 8 Ball says '${eightBall}'`);
